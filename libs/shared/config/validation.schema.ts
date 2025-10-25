ba99c17e3201b54e64ab689609cef3af50fdfbc0
BorsukYevgeniy <akimivdenis2@gmail.com>
2025-10-25 14:37:12 +0300

    refactor: remove required validation for optional configuration fields in validation schema

--- libs/shared/config/validation.schema.ts
+++ libs/shared/config/validation.schema.ts
@@ -2,24 +2,27 @@ import * as joi from 'Joi';
 
 export const validationSchema = joi.object({
   // API-GATEWAY CONFIGURATION
-  API_GATEWAY_PORT: joi.number().port().default(3000).required(),
+  API_GATEWAY_PORT: joi.number().port().default(3000),
   JWT_SECRET: joi.string().required(),
-  JWT_EXPIRATION_TIME: joi.string().pattern(/^\d+\s*(s|m|h|d|w|y)$/i).required(),
+  JWT_EXPIRATION_TIME: joi
+    .string()
+    .pattern(/^\d+\s*(s|m|h|d|w|y)$/i)
+    .required(),
 
   // TIMELOG CONFIGURATION
-  TIMELOG_PORT: joi.number().port().default(3002).required(),
+  TIMELOG_PORT: joi.number().port().default(3002),
 
-  TIMELOG_DB_HOST: joi.string().hostname().default('localhost').required(),
-  TIMELOG_DB_PORT: joi.number().port().default(5432).required(),
+  TIMELOG_DB_HOST: joi.string().hostname().default('localhost'),
+  TIMELOG_DB_PORT: joi.number().port().default(5432),
   TIMELOG_DB_USERNAME: joi.string().required(),
   TIMELOG_DB_PASSWORD: joi.string().required(),
   TIMELOG_DB_NAME: joi.string().required(),
 
   // USERS CONFIGURATION
-  USER_PORT: joi.number().port().default(3001).required(),
+  USER_PORT: joi.number().port().default(3001),
 
-  USER_DB_HOST: joi.string().hostname().default('localhost').required(),
-  USER_DB_PORT: joi.number().port().default(5432).required(),
+  USER_DB_HOST: joi.string().hostname().default('localhost'),
+  USER_DB_PORT: joi.number().port().default(5432),
   USER_DB_USERNAME: joi.string().required(),
   USER_DB_PASSWORD: joi.string().required(),
   USER_DB_NAME: joi.string().required(),
